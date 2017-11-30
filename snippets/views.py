bc53f82bb9bdb3ace16639fb45b371d56e8b9c41
Lucy <ll7@princeton.edu>
2017-11-30 13:18:42 -0500

    DRF tutorial part 4 object level permissions and creating danstan superuser

--- snippets/views.py
+++ snippets/views.py
@@ -2,6 +2,7 @@ from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer, UserSerializer
 from rest_framework import generics, permissions
 from django.contrib.auth.models import User
+from .permissions import IsOwnerOrReadOnly
 
 class SnippetList(generics.ListCreateAPIView):
 	"""
@@ -20,7 +21,8 @@ class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
 	"""
 	queryset = Snippet.objects.all()
 	serializer_class = SnippetSerializer
-	permission_classes = (permissions.IsAuthenticatedOrReadOnly,)
+	permission_classes = (permissions.IsAuthenticatedOrReadOnly,
+						  IsOwnerOrReadOnly,)
 
 class UserList(generics.ListAPIView):
 	queryset = User.objects.all()
