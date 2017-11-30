14659ce4f82df2cc4e51552bb8f69301f6de3994
Lucy <ll7@princeton.edu>
2017-11-30 12:36:48 -0500

    DRF tutorial up to part 3

---
+++ snippets/views.py
@@ -0,0 +1,27 @@
+from snippets.models import Snippet
+from snippets.serializers import SnippetSerializer, UserSerializer
+from rest_framework import generics
+from django.contrib.auth.models import User
+
+class SnippetList(generics.ListCreateAPIView):
+	"""
+	List all snippets, or create a new snippet.
+	"""
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+
+class SnippetDetail(generics.RetrieveUpdateDestroyAPIView):
+	"""
+	Retrieve, update or delete a snippet instance.
+	"""
+	queryset = Snippet.objects.all()
+	serializer_class = SnippetSerializer
+
+class UserList(generics.ListAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
+
+
+class UserDetail(generics.RetrieveAPIView):
+	queryset = User.objects.all()
+	serializer_class = UserSerializer
