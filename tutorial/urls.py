ae55bbeec044af6169dc873eac9eb39a8c47b6f8
Lucy <ll7@princeton.edu>
2017-11-30 13:31:03 -0500

    DRF tutorial part 5 relationships and hyperlinked APIs

--- tutorial/urls.py
+++ tutorial/urls.py
@@ -17,5 +17,4 @@ from django.conf.urls import url, include
 
 urlpatterns = [
 	url(r'^', include('snippets.urls')),
-	url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
